c8356f3d34bb2ae486cdcc4a0965d6ccc017c3bd
William Talley <wtalley@my365.bellevue.edu>
2021-12-06 14:37:18 -0600

    Wk 8 Python calculator

---
+++ week-8/talley_calculator.py
@@ -0,0 +1,26 @@
+# Title: WEB335 Assignment 8.3 Python in Action
+# Date: 28 November, 2021
+# Description: This exercise's objective is to use 
+#Python to create a small calculator functions
+#Source: Tagliaferri, Linda. June 29, 2021. Community. url: https://www.digitalocean.com/community/tutorials/how-to-do-math-in-python-3-with-operators 
+#Source: Stack Overflow. Write a program to find sum of two numbers using functions and calculate their average? August, 2017. 
+#url: https://stackoverflow.com/questions/45529502/write-a-program-to-find-sum-of-two-numbers-using-functions-and-calculate-their-a/45531659 
+
+
+#addition function
+def add(number01, number02):
+    return number01 + number02
+
+#subtraction function
+def subtract(number01, number02):
+    return number01 - number02
+
+#division function
+def divide(number01, number02):
+    return number01/number02
+
+
+#outputs
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
